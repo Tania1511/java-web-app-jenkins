8295cb3dbf98d3760615d95b364b205c8678124e
Tania1511 <taniarajput.scorpio@gmail.com>
2024-05-19 20:34:21 +0530

    controller

--- src/main/java/project/javaWebapp/javawebappjenkins/Contr.java
+++ src/main/java/project/javaWebapp/javawebappjenkins/Contr.java
@@ -2,11 +2,13 @@ package project.javaWebapp.javawebappjenkins;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Contr {
 
     @GetMapping("/get")
+    @ResponseBody
     public String get(){
         return "HEllo jenkins";
     }
